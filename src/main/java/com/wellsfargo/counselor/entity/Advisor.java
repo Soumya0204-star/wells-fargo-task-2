9dd49aa75d8adc220be46bf9090b0bd07a99069a
Soumya <soumya.d2023@vitstudent.ac.in>
2026-02-27 17:34:43 +0530

    Added JPA entities

--- src/main/java/com/wellsfargo/counselor/entity/Advisor.java
+++ src/main/java/com/wellsfargo/counselor/entity/Advisor.java
@@ -1,86 +1,69 @@
 package com.wellsfargo.counselor.entity;
 
-
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 public class Advisor {
-
+    
     @Id
-    @GeneratedValue()
-    private long advisorId;
-
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long advisorId;
+    
     @Column(nullable = false)
     private String firstName;
-
+    
     @Column(nullable = false)
     private String lastName;
-
-    @Column(nullable = false)
-    private String address;
-
-    @Column(nullable = false)
-    private String phone;
-
-    @Column(nullable = false)
+    
+    @Column(nullable = false, unique = true)
     private String email;
-
-    protected Advisor() {
-
-    }
-
-    public Advisor(String firstName, String lastName, String address, String phone, String email) {
+    
+    private String phone;
+    
+    @Column(name = "employee_id", nullable = false, unique = true)
+    private String employeeId;
+    
+    @Temporal(TemporalType.DATE)
+    private Date hireDate;
+    
+    @OneToMany(mappedBy = "advisor", cascade = CascadeType.ALL)
+    private List<Client> clients;
+    
+    protected Advisor() {}
+    
+    public Advisor(String firstName, String lastName, String email, 
+                   String phone, String employeeId, Date hireDate) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.address = address;
-        this.phone = phone;
         this.email = email;
-    }
-
-    public Long getAdvisorId() {
-        return advisorId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
         this.phone = phone;
+        this.employeeId = employeeId;
+        this.hireDate = hireDate;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    
+    // Getters and Setters
+    public Long getAdvisorId() { return advisorId; }
+    
+    public String getFirstName() { return firstName; }
+    public void setFirstName(String firstName) { this.firstName = firstName; }
+    
+    public String getLastName() { return lastName; }
+    public void setLastName(String lastName) { this.lastName = lastName; }
+    
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    
+    public String getEmployeeId() { return employeeId; }
+    public void setEmployeeId(String employeeId) { this.employeeId = employeeId; }
+    
+    public Date getHireDate() { return hireDate; }
+    public void setHireDate(Date hireDate) { this.hireDate = hireDate; }
+    
+    public List<Client> getClients() { return clients; }
+    public void setClients(List<Client> clients) { this.clients = clients; }
 }
---
+++ src/main/java/com/wellsfargo/counselor/entity/Client.java
@@ -0,0 +1,144 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+import java.util.Date;
+
+@Entity
+public class Client {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long clientId;
+
+	@ManyToOne
+	@JoinColumn(name = "advisor_id", nullable = false)
+	private Advisor advisor;
+
+	@Column(nullable = false)
+	private String firstName;
+
+	@Column(nullable = false)
+	private String lastName;
+
+	@Column(nullable = false)
+	private String email;
+
+	@Column(nullable = false)
+	private String phone;
+
+	@Column(nullable = false)
+	private String address;
+
+	@Column(nullable = false)
+	@Temporal(TemporalType.DATE)
+	private Date dateOfBirth;
+
+	@Column(nullable = false)
+	private String ssn;
+
+	@OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+	private Portfolio portfolio;
+
+	protected Client() {
+	}
+
+	public Client(Advisor advisor, String firstName, String lastName, String email, String phone, String address,
+				  Date dateOfBirth, String ssn, Portfolio portfolio) {
+		this.advisor = advisor;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+		this.address = address;
+		this.dateOfBirth = dateOfBirth;
+		this.ssn = ssn;
+		this.portfolio = portfolio;
+	}
+
+	public Long getClientId() {
+		return clientId;
+	}
+
+	public Advisor getAdvisor() {
+		return advisor;
+	}
+
+	public void setAdvisor(Advisor advisor) {
+		this.advisor = advisor;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+	public String getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(String ssn) {
+		this.ssn = ssn;
+	}
+
+	public Portfolio getPortfolio() {
+		return portfolio;
+	}
+
+	public void setPortfolio(Portfolio portfolio) {
+		this.portfolio = portfolio;
+	}
+}
